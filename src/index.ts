a58a10c51c5e4de2326fa95af1219ab143fbb9a5
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-07-28 19:17:02 +0300

    feat: integrate @sifen/jwt-auth-api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDb } from "./configs/mongodb.config";
 dotenv.config();
+import { connectDb } from "./configs/mongodb.config";
+import { auth } from "@sifen/jwt-auth-api";
+import { user } from "@sifen/jwt-auth-api";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(auth());
+app.use(user());
 
 connectDb(() =>
   app.listen(PORT, () => console.log(`server running on port ${PORT}`))
